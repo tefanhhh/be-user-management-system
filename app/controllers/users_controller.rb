97985c8fa3af6123a74d59e793a51f79c9455334
Tefan Haetami <tefanhaetami87@gmail.com>
2023-01-08 16:36:04 +0700

    feat: implement edit, delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email)
